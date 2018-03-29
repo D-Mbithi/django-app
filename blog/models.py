6765c6d0a93ae3884f83c8eb5f46b6bba5fcf609
Dennis Mbithi <denmbithi@gmail.com>
2018-03-29 22:42:45 +0300

    added blog app and created the articles models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextAreaField(max_length=5000)
+    created_at = models.DateTimeField(add_now_time=True)
+    updated = models.DateTimeField(add_now=True)
+    created_by = models.ForeignKey(User, related_name='articles')
+
+    def __str__(self):
+        return self.title
